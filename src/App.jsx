0de70b503970896b162e1792a5fa568d3891ce54
giritharun <163731494+giritharun@users.noreply.github.com>
2025-07-22 16:28:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="skill3">
+    <Router basename="/skill3">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
